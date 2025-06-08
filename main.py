eaeb6dfdfa41a24f69f627dcd3e959e019772c5c
Scottie Gutman <Scottie@signaturecomputer.com>
2025-06-08 15:31:55 -0400

    init

---
+++ main.py
@@ -0,0 +1,31 @@
+# student_dict = {
+#     "student": ["Angela", "James", "Lily"],
+#     "score": [56, 76, 98]
+# }
+
+# #Looping through dictionaries:
+# for (key, value) in student_dict.items():
+#     #Access key and value
+#     pass
+#
+import pandas
+# student_data_frame = pandas.DataFrame(student_dict)
+
+#Loop through rows of a data frame
+# for (index, row) in student_data_frame.iterrows():
+#     #Access index and row
+#     #Access row.student or row.score
+#     pass
+
+# Keyword Method with iterrows()
+# {new_key:new_value for (index, row) in df.iterrows()}
+
+
+nato_df = pandas.read_csv("nato_phonetic_alphabet.csv")
+nato_dict = {row.letter:row.code for (index, row) in nato_df.iterrows()}
+
+target_word = input("please enter the word to be spelled: ").upper()
+target_word_list = [nato_dict[letter] for letter in target_word]
+print(target_word_list)
+
+
